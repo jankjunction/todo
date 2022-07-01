d7708873ff648925a1b2a273ce9088fb0bc9df3e
Micah Bell <micahjoshuabell@gmail.com>
2022-07-01 08:20:34 -0400

    Changed ID generation to use crypto.randomUUID.

--- src/todo.js
+++ src/todo.js
@@ -36,7 +36,7 @@ const deleteToDo = ((project, todo) => {
 });
 
 function toDoID() {
-    let id = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
+    let id = window.crypto.randomUUID();
     return id;
 };
 
